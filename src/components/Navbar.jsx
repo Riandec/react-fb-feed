51604b049af73273d432681c54ea0a1cbb323970
Pitchaya Pimmahasiri <studpitch65@gmail.com>
2025-02-16 20:30:58 +0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,20 @@ import React from "react";
 
 function Navbar() {
     return (
-        <header className="Navbar">Facebook</header>
+        <header className="Navbar">
+            <div className="Logo">
+                Facebook
+            </div>
+            <input type="text" className="SearchBar" placeholder="Search..."/>
+            <ul className="Menu">
+                <li>Home</li>
+                <li><i class="fa-solid fa-user-group"></i></li>
+                <li><i class="fa-brands fa-facebook-messenger"></i></li>
+                <li><i class="fa-solid fa-bell"></i></li>
+                <li>|</li>
+                <li><i class="fa-solid fa-circle-question"></i></li>
+            </ul>
+        </header>
     );
 }
 
